94d33f5183d1067bb41965f477b21cad8b41469b
pushparaj <samant.pushparaj@gmail.com>
2022-03-19 15:37:52 +0800

    Initial Commit

---
+++ HAXOR News/HAXOR_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HAXOR_NewsApp.swift
+//  HAXOR News
+//
+//  Created by Pushparaj Samant on 19/3/22.
+//
+
+import SwiftUI
+
+@main
+struct HAXOR_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
